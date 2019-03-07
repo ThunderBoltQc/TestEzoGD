020dc32bec52714a71531f16136ceae3f33e5733
Guillaume <guiguid_542@hotmail.com>
2019-03-06 20:36:48 -0500

    Ajout des extensions.

---
+++ Calculator/Calculator/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Extensions
+    {
+        public static int Find(this string stringInput, char charToFind)
+        {
+            int pos = 0;
+
+            for (int i = 0; i < stringInput.Length; i++)
+            {
+                if (stringInput[i] == charToFind)
+                {
+                    pos = i;
+                    return pos;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
